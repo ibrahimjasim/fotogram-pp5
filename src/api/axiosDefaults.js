73f1756da094c68ab9ecd253bb1b6c7562afa947
ibrahimjasim <>
2023-12-15 16:15:25 +0000

    created axios interceptors axiosRes and axiosReq, set axios to refresh access_token if it gets a 401 error

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api1-df01c14b5fdd.herokuapp.com/";
 axios.defaults.headers.post['Content-Type'] ='multipart/form-data';
 axios.defaults.withCredentials =true;
+
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
