ac49e8ccd371883f2eefb6d36ac569335c941a5e
ibrahimjasim <>
2023-12-15 15:45:21 +0000

    Finshing the signup methohd, the user now can create a new account

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
-    return (
-      <div className={styles.App}>
-        <NavBar />
-        <Container className={styles.Main}>
-          <Switch>
-            <Route exact path="/" render={() => <h1>Home page</h1>} />
-            <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-            <Route exact path="/signup" render={() =><SignUpForm />} />
-            <Route render={() => <p>Page not found!</p>} />
-          </Switch>
-        </Container>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
