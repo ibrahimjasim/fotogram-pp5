f5914d0143594f96cc5a6eae848f0ae81989aadc
ibrahimjasim <>
2023-12-19 19:27:01 +0000

    Created a PopularProfiles file and immported react and appStyles

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import appStyles from '../../App.module.css'
+
+const PopularProfiles = () => {
+  return (
+    <div>PopularProfiles</div>
+  )
+}
+
+export default PopularProfiles
