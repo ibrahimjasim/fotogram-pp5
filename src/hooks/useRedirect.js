2aab88a1ddd6198ec70ab21c20c4aa9c81048165
ibrahimjasim <>
2023-12-26 20:15:16 +0000

    imported useRedirect to signInForm

---
+++ src/hooks/useRedirect.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+export const useRedirect = (userAuthStatus) => {
+    // Redirect the user to the home page
+    const history = useHistory();
+
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+            // if user is logged in, the code below will run
+            if (userAuthStatus === "loggedIn") {
+              history.push("/");
+            }
+          } catch (err) {
+            // if user is not logged in, the code below will run
+            if (userAuthStatus === "loggedOut") {
+              history.push("/");
+            }
+          }
+        };
+    
+        handleMount();
+      }, [history, userAuthStatus]);
+    };
